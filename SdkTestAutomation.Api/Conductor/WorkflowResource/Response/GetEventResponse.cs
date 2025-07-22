54fd4d2ade57173dcd65af4992a52c5e5e3658f7
EugeneKisel-sm <eugene.kisel@siliconmint.com>
2025-07-22 15:05:09 +0200

    Fix target framework compatibility and remove duplicate file

--- SdkTestAutomation.Api/Conductor/WorkflowResource/Response/GetEventResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace SdkTestAutomation.Api.Conductor.WorkflowResource.Response;
-
-public class GetWorkflowResponse
-{
-    public string Status { get; set; }
-    public string WorkflowId { get; set; }
-}
